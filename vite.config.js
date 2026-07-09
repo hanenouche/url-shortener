05b003cd57f50e250b3a543f157e6032c6062c28
Hanene Ait Laoussine <eh_ait_laoussine@esi.dz>
2026-07-09 08:00:21 +0100

    🚀 deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // served from https://hanenouche.github.io/url-shortener/
+  base: '/url-shortener/',
   plugins: [react()],
   resolve: {
     alias: {
